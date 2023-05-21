4f95cf369e8131a56976d2eec803091e51ab20bb
bwj2800 <bwj2800@naver.com>
2023-05-21 22:24:09 +0900

    feat: matching websites and crawler functions by id

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ urls = db.read_url()
 for i in range(len(urls)):
     url = urls.loc[i, 'url']
     headers = urls.loc[i, 'headers']
-    func_name = "crawl_"+str(i)
+    id = urls.loc[i, 'id']
+    func_name = "crawl_"+str(id)
     website_crawler = getattr(crawler, func_name)
     website_crawler(i, url, headers)
     print("finished crawling for " +str(i)+ " website\n")
