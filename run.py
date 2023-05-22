087469e9fe6a4a824606da9ccf1e168982e409ab
bwj2800 <bwj2800@naver.com>
2023-05-22 23:09:30 +0900

    feat: add scheduler

--- run.py
+++ run.py
@@ -1,18 +1,34 @@
 from src.combinated_crawler import Crawler
 from src.db_connecter import DB
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-crawler = Crawler()
-db = DB()
-urls = db.read_url()
-
-for i in range(len(urls)):
-    url = urls.loc[i, 'url']
-    headers = urls.loc[i, 'headers']
-    id = urls.loc[i, 'id']
-    func_name = "crawl_"+str(id)
+def crawling(cur_idx):
+    url = urls.loc[cur_idx, 'url']
+    headers = urls.loc[cur_idx, 'headers']
+    id = urls.loc[cur_idx, 'id']
+    func_name = "crawl_"+str(id) #website id와 crawler 함수 이름 일치
     website_crawler = getattr(crawler, func_name)
-    website_crawler(i, url, headers)
-    print("finished crawling for " +str(i)+ " website\n")
+    website_crawler(cur_idx, url, headers)
 
-    db.insert_data(i)
+    # db.insert_data(cur_id)
+    
+def circuit():
+    for i in range(len(urls)):
+        for retry in range(2): #에러 발생시 재시도 횟수
+            try:
+                crawling(i)
+                print("finished crawling for website index " +str(i)+"\n")
+                break
+            except:
+                print("failed crawling for website index " +str(i)+"\n")
+            
+      
+if __name__ == "__main__":
+    crawler = Crawler()
+    db = DB()
+    urls = db.read_url()
+    
+    sched = BlockingScheduler(timezone='Asia/Seoul')
+    sched.add_job(circuit, 'cron', hour=0, minute=20) #매일 hour시 minute분에 실행
+    sched.start()
 
